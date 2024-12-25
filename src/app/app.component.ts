beda4ae63a410772cc2e4a8162d6ed04c993cd9a
Hamza Oumeziane <118761133+HamzaOumeziane@users.noreply.github.com>
2024-12-24 22:06:11 -0500

    Minimal component in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello World!';
+  name = 'Pikachu';
+  life = 23;
+
+  incrementLife(){
+    console.log('incrementing life');
+    alert('incrementing life');
+    this.life += 1;
+  }
+
+  decrementLife(){
+    console.log('decrementing life');
+    alert('decrementing life');
+    this.life -= 1;
+  }
 }
